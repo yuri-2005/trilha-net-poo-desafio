b8fa04d7367f934ed9cc3039cbe937ab901b0b5f
Eliane Turcilio <yuritb2008@gmail.com>
2025-03-26 21:29:27 -0300

    Finalização Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Smartphone nokiaG32 = new Nokia("42904", "Modelo G32", "236714", 32);
+nokiaG32.Ligar();
+nokiaG32.InstalarAplicativo("Netflix");
+
+Console.WriteLine("------------------");
+
+Console.WriteLine("Iphone: ");
+Smartphone iphone7Pro = new Iphone("87123", "Modelo 7 Pro", "44402932", 64);
+iphone7Pro.Ligar();
+iphone7Pro.InstalarAplicativo("Linkedin");
